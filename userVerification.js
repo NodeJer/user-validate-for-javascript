ce8b10a4e06bb86ff6d0f098a614a43734db294c
Cole <1245089789@qq.com>
2014-11-28 11:02:14 +0800

    Update userVerification.js

--- userVerification.js
+++ userVerification.js
@@ -11,7 +11,7 @@
   }
 })(window, window.Zepto||window.jQuery, function($){
 
-  function userVerification(string, error, success){
+  function userVerification(string, success, error){
     var email = /\w+((-w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+/.test(string);
     var phone = /0?(13|14|15|18)[0-9]{9}/.test(string);
     var userName = /^[A-Za-z0-9_\-\u4e00-\u9fa5]{6,25}$/.test(string);
